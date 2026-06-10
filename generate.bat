d8a85f8e65f0f21c76eaab6a9a084f2d880f6e75
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2026-06-10 17:43:42 -0400

    Patch generated project output name to swiflyboiii

--- generate.bat
+++ generate.bat
@@ -1,3 +1,4 @@
 @echo off
 git submodule update --init --recursive
 tools\premake5 %* vs2022
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-ChildItem -Path build -Recurse -Filter *.vcxproj | ForEach-Object { $p = $_.FullName; $c = Get-Content $p -Raw; $c = $c -replace '<TargetName>boiii</TargetName>', '<TargetName>swiflyboiii</TargetName>'; Set-Content -Path $p -Value $c -NoNewline }"
