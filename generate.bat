754ff791362a1fb91a2ff2a1905459ccde0bf7f6
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2026-06-10 19:25:47 -0400

    Remove extra icon generation from build script

--- generate.bat
+++ generate.bat
@@ -1,10 +1,4 @@
 @echo off
 git submodule update --init --recursive
-if exist src\client\resources\icon.ico.b64 (
-  powershell -NoProfile -ExecutionPolicy Bypass -Command "$b64 = Get-Content 'src/client/resources/icon.ico.b64' -Raw; [IO.File]::WriteAllBytes('src/client/resources/icon.ico.raw', [Convert]::FromBase64String($b64))"
-)
-if exist tools\make_png_ico.ps1 if exist src\client\resources\icon.ico.raw (
-  powershell -NoProfile -ExecutionPolicy Bypass -File tools\make_png_ico.ps1 -InputPath src\client\resources\icon.ico.raw -OutputPath src\client\resources\icon.ico
-)
 tools\premake5 %* vs2022
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-ChildItem -Path build -Recurse -Filter *.vcxproj | ForEach-Object { $p = $_.FullName; $c = Get-Content $p -Raw; $c = $c -replace '<TargetName>boiii</TargetName>', '<TargetName>swiflyboiii</TargetName>'; if ($c -notmatch 'Wno-deprecated-declarations') { $c = $c -replace '</ClCompile>', '  <AdditionalOptions>-Wno-deprecated-declarations %(AdditionalOptions)</AdditionalOptions>' + [Environment]::NewLine + '</ClCompile>' }; Set-Content -Path $p -Value $c -NoNewline }"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-ChildItem -Path build -Recurse -Filter *.vcxproj | ForEach-Object { $p = $_.FullName; $c = Get-Content $p -Raw; $c = $c -replace '<TargetName>boiii</TargetName>', '<TargetName>swiflyboiii</TargetName>'; Set-Content -Path $p -Value $c -NoNewline }"
