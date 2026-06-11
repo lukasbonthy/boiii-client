e53824579451599fbf262a60eee94461a19de4f8
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2026-06-11 02:54:19 -0400

    Enable Swifly updater runner

--- src/client/updater/updater.cpp
+++ src/client/updater/updater.cpp
@@ -1,10 +1,17 @@
 #include <std_include.hpp>
 
 #include "updater.hpp"
+#include "updater_ui.hpp"
+#include "file_updater.hpp"
 
 namespace updater {
-void run(const std::filesystem::path & /*base*/) {
-  // Swifly does not use the upstream BOIII/Ezz updater. Keep this as a no-op so
-  // no caller can reach file_updater or the old r2.ezz.lol update channel.
+void run(const std::filesystem::path &base) {
+  const auto self = utils::nt::library::get_by_address(run);
+  const auto self_file = self.get_path();
+
+  updater_ui updater_ui{};
+  const file_updater file_updater{updater_ui, base, self_file};
+
+  file_updater.run();
 }
 } // namespace updater
